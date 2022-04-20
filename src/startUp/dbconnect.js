7a302fb3c878baa6e9be2c98df4003f8a59708c0
Ronit Harkhani <86475077+ronit-tm@users.noreply.github.com>
2022-04-20 11:43:45 +0530

    test pusing

--- src/startUp/dbconnect.js
+++ src/startUp/dbconnect.js
@@ -6,11 +6,17 @@ if (!config.jwtPrivatKey){
     console.error('FATAL ERROR: jwtPrivateKey is not defined');
     process.exit(1);
 }
+console.log('Database :>> ', config.DB_URL);
 mongoose.connect(config.DB_URL,  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('Connected to MongoDB..................'))
-.catch(err => console.error('Could not connect to Mongodb...', err));
+.then(() =>{
+    
+    console.log('Connected to MongoDB..................')
+    
+} ).catch((err) => {
+    console.error('Could not connect to Mongodb...', err)
+}) 
 
 exports.mongoose = mongoose;
